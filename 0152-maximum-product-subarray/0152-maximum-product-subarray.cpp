b131edb0f08042a78ab526dd0147227604fa6a08
UdbhavUry <yadudbhav911@gmail.com>
2026-08-27 14:58:47 +0530

    Time: 0 ms (100%), Space: 17.8 MB (23.41%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n=nums.size();
-        int maxproduct=INT_MIN;
-        for(int i=0;i<n;i++){
-             
-            int product=1;
-            for(int j=i;j<n;j++){
-                product*=nums[j];
-                maxproduct=max(product,maxproduct);
+        int maxProd = nums[0];
+        int minProd = nums[0];
+        int ans = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            int num = nums[i];
+
+            // Agar number negative hai, swap kar do
+            if (num < 0) {
+                swap(maxProd, minProd);
             }
-        } return maxproduct;
-        
+
+            // Update max and min product
+            maxProd = max(num, num * maxProd);
+            minProd = min(num, num * minProd);
+
+            // Update answer
+            ans = max(ans, maxProd);
+        }
+
+        return ans;
     }
 };
